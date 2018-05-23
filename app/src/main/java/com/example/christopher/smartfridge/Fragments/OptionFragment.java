ed90b4b8387a8438bdff62e41f88292c776db846
SukiCst <jmlt97@t-online.de>
2018-05-23 11:34:17 +0200

    erstes Settings-Saven

--- app/src/main/java/com/example/christopher/smartfridge/Fragments/OptionFragment.java
+++ app/src/main/java/com/example/christopher/smartfridge/Fragments/OptionFragment.java
@@ -10,10 +10,16 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Switch;
 
+import com.example.christopher.smartfridge.OrmDataHelper;
 import com.example.christopher.smartfridge.R;
+import com.example.christopher.smartfridge.SettingsItem;
 
 public class OptionFragment extends Fragment {
-
+    Switch autofocus;
+    Switch lightning;
+    Switch notifications;
+    EditText textsize;
+    Button saveSettingsButton;
     public static OptionFragment newInstance(String text) {
         OptionFragment optionFragment = new OptionFragment();
         Bundle bundle = new Bundle();
@@ -30,13 +36,31 @@ public class OptionFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.activity_settings, container, false);
-        Switch autofocus = view.findViewById(R.id.autofocus);
-        Switch lightning = view.findViewById(R.id.lightning);
-        Switch notifications = view.findViewById(R.id.notifications);
-        EditText textsize = view.findViewById(R.id.textsize);
-        Button saveSettings = view.findViewById(R.)
+        autofocus = view.findViewById(R.id.autofocus);
+        lightning = view.findViewById(R.id.lightning);
+        notifications = view.findViewById(R.id.notifications);
+        textsize = view.findViewById(R.id.textsize);
+        saveSettingsButton = view.findViewById(R.id.saveSettings);
+        saveSettingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveSettings();
+            }
+        });
         return view;
     }
 
+        public void saveSettings(){
+            OrmDataHelper helper = new OrmDataHelper(getActivity());
+            if(helper.getSettingItem() != null){
+                helper.deleteSettingItem(helper.getSettingItem());
+            }
+            SettingsItem settings = new SettingsItem();
+            settings.setAutofocus(autofocus.isChecked());
+            settings.setLightning(lightning.isChecked());
+            settings.setNotifications(notifications.isChecked());
+            settings.setTextSize(Integer.parseInt(textsize.getText().toString()));
 
+            helper.saveSettingItem(settings);
+        }
 }
