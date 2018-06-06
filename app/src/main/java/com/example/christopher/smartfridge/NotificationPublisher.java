ca54a6b85f9a92e11251ccd38c160ecfd84d7613
ChristopherPS96 <M1WmdNBind!>
2018-06-06 12:10:32 +0200

    Version 1.43 Bugfixes

--- app/src/main/java/com/example/christopher/smartfridge/NotificationPublisher.java
+++ app/src/main/java/com/example/christopher/smartfridge/NotificationPublisher.java
@@ -3,39 +3,18 @@ package com.example.christopher.smartfridge;
 import android.annotation.TargetApi;
 import android.app.AlarmManager;
 import android.app.Notification;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
 import android.app.PendingIntent;
-import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.SystemClock;
 import android.support.v4.app.NotificationCompat;
 
 import java.util.Calendar;
-import java.util.Date;
 
-public class NotificationPublisher extends BroadcastReceiver {
+public class NotificationPublisher {
 
     public static String NOTIFICATION_ID = "notification-id";
     public static String NOTIFICATION = "notification";
-
-    @Override
-    public void onReceive(Context context, Intent intent) {
-        try {
-            NotificationChannel channel;
-            NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-                channel = new NotificationChannel("123", "Channel", NotificationManager.IMPORTANCE_DEFAULT);
-                notificationManager.createNotificationChannel(channel);
-            }
-            Notification notification = intent.getParcelableExtra(NOTIFICATION);
-            int id = intent.getIntExtra(NOTIFICATION_ID, 0);
-            notificationManager.notify(id, notification);
-        } catch (NullPointerException e) {
-            e.getStackTrace();
-        }
-    }
 
     public void scheduleNotification(BestandItem bestandItem, Context context) {
         OrmDataHelper helper = new OrmDataHelper(context);
@@ -45,7 +24,7 @@ public class NotificationPublisher extends BroadcastReceiver {
             isNotification = settingsItem.isNotifications();
         }
         if(isNotification){
-            Intent intent = new Intent(context, NotificationPublisher.class);
+            Intent intent = new Intent(context, NotificationReceiver.class);
             intent.putExtra(NotificationPublisher.NOTIFICATION_ID, bestandItem.getId());
             intent.putExtra(NotificationPublisher.NOTIFICATION, getNotification(bestandItem.getScanItem().getName(), context));
             PendingIntent pendingIntent = PendingIntent.getBroadcast(context, bestandItem.getId(), intent, PendingIntent.FLAG_UPDATE_CURRENT);
